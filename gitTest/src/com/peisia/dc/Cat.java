f23af4167005b4ff68b785e3cecd5925a3fd8ba2
DuckchiS <rjsdn1862@daum.net>
2024-03-21 09:22:46 +0900

    inital commit

---
+++ gitTest/src/com/peisia/dc/Cat.java
@@ -0,0 +1,5 @@
+package com.peisia.dc;
+
+public class Cat {
+
+}
---
+++ gitTest/src/com/peisia/dc/main.java
@@ -0,0 +1,7 @@
+package com.peisia.dc;
+
+public class main {
+	public static void main(String[] args) {
+		System.out.println("깃 테스트");
+	}
+}
